482b2c0e484915e1efefd60a2f0b1e1d8dfdf86e
jaafar2000 <jaafar200021@gmail.com>
2023-02-20 13:01:44 +0530

    trusted by component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import Featured from '../../components/featured/Featured'
+import TrustedBy from '../../components/trustedBy/TrustedBy'
 import './home.scss'
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home">
+    <Featured />
+    <TrustedBy/>
+  </div>
   )
 }
 
